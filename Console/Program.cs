20c79ed512e092c99d82c51b2c87bfbb721877cc
Esma Ekim <esmaekim123@gmail.com>
2024-10-02 19:46:29 +0300

    Datenbankfehler behoben

--- Console/Program.cs
+++ Console/Program.cs
@@ -60,7 +60,7 @@ namespace ConsoleUI
 
             foreach (var car in carManager.GetCarDetails())
             {
-                Console.WriteLine(car.CarName + "/" + car.BrandName);
+                Console.WriteLine(car.CarName + "/" + car.BrandName +"/"+ car.ColorName);
             }
         }
     }
--- DataAccess/Concrete/EntityFramework/EfCarDal.cs
+++ DataAccess/Concrete/EntityFramework/EfCarDal.cs
@@ -18,9 +18,9 @@ namespace DataAccess.Concrete.EntityFramework
             using (CarContext context = new CarContext())
             {
                 var result = from c in context.Cars
-                             join b in context.Brands
-                             on c.CarId equals b.BrandId
-                             select new CarDetailDto {CarId=c.CarId, CarName=c.CarName, BrandName=b.BrandName, ModelYear= c.ModelYear, DailyPrice= c.DailyPrice, Description=c.Description  };
+                             join b in context.Brands on c.CarId equals b.BrandId
+                             join co in context.Colors on c.CarId equals co.ColorId
+                             select new CarDetailDto {CarId=c.CarId, CarName=c.CarName, BrandName=b.BrandName, ModelYear= c.ModelYear, DailyPrice= c.DailyPrice, Description=c.Description, ColorName= co.ColorName  };
                 return result.ToList();
             }
         }
